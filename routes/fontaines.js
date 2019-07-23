86c9cfea927ed42782f9eb0829a253c766313640
Camille Jacques <camille.jacques75@gmail.com>
2019-07-23 09:52:20 +0200

    added patch route

--- routes/fontaines.js
+++ routes/fontaines.js
@@ -54,4 +54,14 @@ router.delete("/:id", (req, res) => {
     .catch(dbErr => res.send(dbErr));
 });
 
+router.patch("/:id", (req, res) => {
+  console.log("YOYOYO", req.body);
+  Fontaine.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(dbRes => {
+      console.log(dbRes);
+      res.status(200).json(dbRes);
+    })
+    .catch(dbErr => res.send(dbErr));
+});
+
 module.exports = router;
