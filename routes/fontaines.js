c03cf7701795eb8d95091941bfbc51376cb495c4
Cecile Pham <sheshiru.van@gmail.com>
2019-07-24 11:41:41 +0200

    pagination

--- routes/fontaines.js
+++ routes/fontaines.js
@@ -5,7 +5,12 @@ const filterData = require("../seeds/feedMyDb");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(req.query.offset);
+  const offset = req.query.offset ? Number(req.query.offset) : 0;
+  const limit = offset ? 40 : 0;
   Fontaine.find()
+    .skip(offset)
+    .limit(limit)
     .then(fontaines => {
       res.json(fontaines);
     })
